aa1a1e95121ee117a003433570cfeaf165dbb59b
Jamie <jamiespekman@hotmail.com>
2023-03-26 15:49:18 +0200

    Enable global cors

--- src/main/java/com/example/controller/ContactInfoController.java
+++ src/main/java/com/example/controller/ContactInfoController.java
@@ -3,6 +3,7 @@ package com.example.controller;
 import com.example.model.ContactInfo;
 //import com.example.repository.ContactInfoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,8 +23,10 @@ public class ContactInfoController {
 
 
     @GetMapping("")
+
     public List<ContactInfo> getAllContactInfo() {
-        return List.of(new ContactInfo(1,"Jamie","Spekman","landingsoft@hotmail.com"));
+        return List.of(new ContactInfo(1,"Jamie","Spekman","landingsoft@hotmail.com"),
+                        new ContactInfo(2,"Dwayne","Johnson", "TheRock@hollywood.com"));
     }
 
 
