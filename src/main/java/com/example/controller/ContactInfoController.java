70b2a884fef1302deb07bbf62b800b5be08e6840
Jamie <jamiespekman@hotmail.com>
2023-03-29 11:47:51 +0200

    pipeline change14

--- src/main/java/com/example/controller/ContactInfoController.java
+++ src/main/java/com/example/controller/ContactInfoController.java
@@ -25,7 +25,7 @@ public class ContactInfoController {
     @GetMapping("")
     public List<ContactInfo> getAllContactInfo() {
         return List.of(new ContactInfo(1,"Jamie","Spekman","landingsoft@hotmail.com"),
-                        new ContactInfo(2,"D","Johnson", "TheRock@hollywood.com"),
+                        new ContactInfo(2,"Dwayne","Johnson", "TheRock@hollywood.com"),
                         new ContactInfo(3,"M","Lemonis", "Marcus@TheProfit.com"));
 // change
 //        return List.of(new ContactInfo(1,"Jamie","Spekman","landingsoft@hotmail.com"),
