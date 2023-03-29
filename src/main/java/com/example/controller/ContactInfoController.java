9097f58c217a192775f271892164c4728f6d3cab
Jamie <jamiespekman@hotmail.com>
2023-03-29 09:18:15 +0200

    Extra routing

--- src/main/java/com/example/controller/ContactInfoController.java
+++ src/main/java/com/example/controller/ContactInfoController.java
@@ -23,12 +23,17 @@ public class ContactInfoController {
 
 
     @GetMapping("")
-
     public List<ContactInfo> getAllContactInfo() {
         return List.of(new ContactInfo(1,"Jamie","Spekman","landingsoft@hotmail.com"),
                         new ContactInfo(2,"Dwayne","Johnson", "TheRock@hollywood.com"),
                         new ContactInfo(3,"Marcus","Lemonis", "Marcus@TheProfit.com"));
     }
+
+    public ContactInfo getById() {
+        return new ContactInfo(1,"Sandra","Bullock","Sandra@Hollywwod");
+    }
+
+
 
 
 }
